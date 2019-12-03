db12cea3cee21ec8ca83ed754cba0c0de0833e07
Adam Bowen <popewv@gmail.com>
2019-12-02 20:58:21 -0500

    build

---
+++ scripts/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+SCRIPTSDIR=$(pwd)
+ROOTDIR=${SCRIPTSDIR}/..
+
+cp _exclude.yml ${ROOTDIR}
+
+# for version in $(ls ../archive); do
+#     yq w -i ${ROOTDIR}/_exclude.yml "exclude[+]" "${version}"
+# done
+
+cd ${ROOTDIR}
+
+bundle exec jekyll build --config _config.yml,_exclude.yml
+
+rm _exclude.yml
